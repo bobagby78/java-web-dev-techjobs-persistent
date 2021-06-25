6b9103154bf27ecdd4b1ea9e2685848a90b3768e
bagbyj <bagbyj@wesclin.org>
2021-06-24 20:19:13 -0500

    Part 1 complete. Part 2-1 (AbstractEntity) complete.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,12 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotEmpty (message = "The name field is required")
+    @Size (min= 3, max =250, message = "The name must be between 3 and 250 characters" )
     private String name;
 
     public int getId() {
