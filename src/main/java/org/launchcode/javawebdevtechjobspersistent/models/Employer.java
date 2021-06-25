e083c8d0ad98c99d2f7f64c42e940deb3ab376d3
bagbyj <bagbyj@wesclin.org>
2021-06-24 20:33:28 -0500

    Done with the "models" part of section 2.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotEmpty (message = "Location is a required field.")
+    @Size (min =3, max = 50, message = "Location needs to be between 3 an 50 characters")
+    private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer(){};
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotEmpty(message = "Please be sure to enter required job skills")
+    @Size(min = 3, max = 500, message = "Not to exceed 500 characters")
+    private String necessarySkills;
+
+    public Skill(String necessarySkills) {
+        this.necessarySkills = necessarySkills;
+    }
+    public Skill() {};
+
+    public String getNecessarySkills() {
+        return necessarySkills;
+    }
+
+    public void setNecessarySkills(String necessarySkills) {
+        this.necessarySkills = necessarySkills;
+    }
 }
