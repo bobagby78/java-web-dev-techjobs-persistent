4ade31c66f850096a13dddf0190f4a54598f0cdf
bagbyj <bagbyj@wesclin.org>
2021-06-25 11:21:09 -0500

    Data layer complete. repositories created for employer and skill classes.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
@@ -1,13 +1,13 @@
 package org.launchcode.javawebdevtechjobspersistent.models.data;
 
-import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import javax.transaction.Transactional;
 
+
 @Repository
-@Transactional
-public interface JobRepository extends CrudRepository<Job, Integer> {
+//@Transactional read up, buttercup
+public interface EmployerRepository extends CrudRepository<Employer, Integer> {
 
 }
